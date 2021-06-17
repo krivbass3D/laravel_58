0ebdb9a8216388d876634fa690a49f35bc7d1918
besik-74 <sergej.belaj.kr@gmail.com>
2021-06-17 17:09:48 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,31 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return view('welcome');});
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'],function (){
+  Route::resource('posts', 'PostController')->names('blog.posts');
+});
+//> Админка Блога
+$gropData =[
+  'namespace'   => 'Blog\Admin',
+  'prefix'      => 'admin/blog',
+];
+
+Route::group($gropData, function (){
+    //BlogCategory
+    $methods = ['index', 'edit', 'update', 'create', 'store',];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+
+    //BlogPost
+
+    Route::resource('posts', 'PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//<
